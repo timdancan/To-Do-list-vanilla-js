f15a08b28ff0b66a1501eab5a0fa9a8c4c7f4517
timdancan <saqe789@gmail.com>
2021-03-11 14:11:16 +0200

    страховка

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,7 @@ const successfulTaskNumber = document.querySelector(
   ".counter__number_successful"
 );
 const date = formattedDate();
+const todoList = []
 
 function composeItem() {
   const newItem = template.content.cloneNode(true);
@@ -15,6 +16,7 @@ function composeItem() {
   const deleteButton = newItem.querySelector(".task__button_trash");
   const editButton = newItem.querySelector(".task__button_edit");
   const taskText = newItem.querySelector(".task__text");
+  // Удаление элемента
   deleteButton.addEventListener("click", (e) => {
     const targetElement = e.target;
     const targetItem = targetElement.closest(".task__label");
@@ -27,13 +29,16 @@ function composeItem() {
       activeTaskNumber.textContent = reduceCount(prevCountActive);
     }
     scopeTaskNumber.textContent = reduceCount(prevCountScope);
+    removeLocalTodos(targetItem)
     targetItem.remove();
   });
+  //  Изменение импута
   editButton.addEventListener("click", () => {
     editButton.classList.toggle("task__button_edit-active");
     taskText.disabled = !taskText.disabled;
     taskText.classList.toggle("task__text_disabled");
   });
+  // Тогл активный - не активный
   checkboxStyle.addEventListener("click", () => {
     taskText.classList.toggle("task__text_active");
     checkboxStyle.classList.toggle("task__checkbox_active");
@@ -47,25 +52,18 @@ function composeItem() {
       successfulTaskNumber.textContent = reduceCount(prevCountSuccess);
     }
   });
+
   taskData.textContent = date;
+  // добавлние в локалсторедж
+  saveLocalTodos(taskText.value);
+
   return newItem;
 }
-
-// function deleteItem(e) {
-//   const targetElement = e.target;
-//   const targetItem = targetElement.closest(".task__label");
-//   scopeTaskNumber.textContent = reduceCount(prevCountScope);
-//   if (checkboxStyle.classList.contains("task__checkbox_active")) {
-//     activeTaskNumber.textContent = reduceCount(prevCountActive);
-//   } else {
-//     successfulTaskNumber.textContent = reduceCount(prevCountSuccess);
-//   }
-//   targetItem.remove();
-// }
 
 function addNewItem() {
   const newItem = composeItem();
   task.prepend(newItem);
+
   const prevCountScope = scopeTaskNumber.textContent;
   const prevCountActive = activeTaskNumber.textContent;
   scopeTaskNumber.textContent = increaseCount(prevCountScope);
@@ -83,15 +81,100 @@ function reduceCount(prev) {
 }
 
 function formattedDate(d = new Date()) {
-  let month = String(d.getMonth() + 1);
+  let month = String(d.toLocaleString("default", { month: "long" }));
   let day = String(d.getDate());
-  const year = String(d.getFullYear());
+  let year = String(d.getFullYear());
 
   if (month.length < 2) month = "0" + month;
   if (day.length < 2) day = "0" + day;
 
   return `${day}/${month}/${year}`;
 }
+
+function saveLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getTodos() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach((todo) => {
+    const newItem = template.content.cloneNode(true);
+    const taskData = newItem.querySelector(".task__data");
+    const checkboxStyle = newItem.querySelector(".checkbox__style");
+    const deleteButton = newItem.querySelector(".task__button_trash");
+    const editButton = newItem.querySelector(".task__button_edit");
+    const taskText = newItem.querySelector(".task__text");
+    // Удаление элемента
+    deleteButton.addEventListener("click", (e) => {
+      const targetElement = e.target;
+      const targetItem = targetElement.closest(".task__label");
+      const prevCountScope = scopeTaskNumber.textContent;
+      const prevCountActive = activeTaskNumber.textContent;
+      const prevCountSuccess = successfulTaskNumber.textContent;
+      if (checkboxStyle.classList.contains("task__checkbox_active")) {
+        successfulTaskNumber.textContent = reduceCount(prevCountSuccess);
+      } else {
+        activeTaskNumber.textContent = reduceCount(prevCountActive);
+      }
+      scopeTaskNumber.textContent = reduceCount(prevCountScope);
+      removeLocalTodos()
+      targetItem.remove();
+    });
+    //  Изменение импута
+    editButton.addEventListener("click", () => {
+      editButton.classList.toggle("task__button_edit-active");
+      taskText.disabled = !taskText.disabled;
+      taskText.classList.toggle("task__text_disabled");
+    });
+    // Тогл активный - не активный
+    checkboxStyle.addEventListener("click", () => {
+      taskText.classList.toggle("task__text_active");
+      checkboxStyle.classList.toggle("task__checkbox_active");
+      const prevCountActive = activeTaskNumber.textContent;
+      const prevCountSuccess = successfulTaskNumber.textContent;
+      if (checkboxStyle.classList.contains("task__checkbox_active")) {
+        activeTaskNumber.textContent = reduceCount(prevCountActive);
+        successfulTaskNumber.textContent = increaseCount(prevCountSuccess);
+      } else {
+        activeTaskNumber.textContent = increaseCount(prevCountActive);
+        successfulTaskNumber.textContent = reduceCount(prevCountSuccess);
+      }
+    });
+
+    taskData.textContent = date;
+
+    task.prepend(newItem);
+
+    const prevCountScope = scopeTaskNumber.textContent;
+    const prevCountActive = activeTaskNumber.textContent;
+    scopeTaskNumber.textContent = increaseCount(prevCountScope);
+    activeTaskNumber.textContent = increaseCount(prevCountActive);
+  });
+}
+
+function removeLocalTodos() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  localStorage.removeItem("todos")
+}
+
+document.addEventListener("DOMContentLoaded", getTodos);
 
 addButton.forEach((button) => {
   button.addEventListener("click", addNewItem);
